a3c7afde1c9614f2df2c5348ad478dfdcd22fb46
Sean Odnert <sosharkie@gmail.com>
2018-10-16 10:27:27 -0700

    Added base web2py app with initial login functionality and 4 empty pages

---
+++ SurfsUpApp/static/js/default_index.js
@@ -0,0 +1,43 @@
+// This is the js for the default/index.html view.
+
+var app = function() {
+
+    var self = {};
+
+    Vue.config.silent = false; // show all warnings
+
+    // Extends an array
+    self.extend = function(a, b) {
+        for (var i = 0; i < b.length; i++) {
+            a.push(b[i]);
+        }
+    };
+
+    self.change_page = function(new_page) {
+        console.log(new_page);
+        self.vue.page = new_page;
+    }
+
+    // Complete as needed.
+    self.vue = new Vue({
+        el: "#vue-div",
+        delimiters: ['${', '}'],
+        unsafeDelimiters: ['!{', '}'],
+        data: {
+            page: 'homepage',
+        },
+        methods: {
+            change_page: self.change_page,
+        }
+
+    });
+
+
+    return self;
+};
+
+var APP = null;
+
+// This will make everything accessible from the js console;
+// for instance, self.x above would be accessible as APP.x
+jQuery(function(){APP = app();});
