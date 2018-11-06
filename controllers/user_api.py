854376a0461ba91b2bdd2c350970052f840ad38f
Sean Odnert <sosharkie@gmail.com>
2018-11-06 15:56:14 -0800

    Divided api into 2 files

--- controllers/user_api.py
+++ controllers/user_api.py
@@ -1,5 +1,3 @@
-# The file containing all the API methods
-
 # Gets all the created users from the auth user table
 def get_users():
 	current_user = None
@@ -101,70 +99,3 @@ def delete_all_user_data():
 	"Deletes all users from the table"
 	db(db.user_data.id > -1).delete()
 	return "ok"
-
-def change_group_name():
-    row = db(db.group_data.id == request.vars.group_id).select().first()
-    row.update_record(group_name = request.vars.group_name)
-    return "ok"
-
-def add_group():
-    t_id = db.group_data.insert(
-	    group_owner = request.vars.group_owner,
-        group_name = request.vars.group_name,
-	)
-    r = db(db.group_data.id == t_id).select().first()
-    return response.json(dict(group_data=dict(
-		id = r.id,
-        group_owner = r.group_owner,
-		group_name = r.group_name,
-        members = r.members
-    )))
-
-def delete_all_group_data():
-	"Deletes all groups from the table"
-	db(db.group_data.id > -1).delete()
-	return "ok"
-
-def delete_group():
-    db(db.group_data.id == request.vars.group_id).delete()
-    return "ok"
-
-def get_groups():
-    groups = []
-    for r in db(db.group_data.group_owner == request.vars.group_owner).select():
-        t = dict(
-            id = r.id,
-            group_owner = r.group_owner,
-            group_name = r.group_name,
-            members = r.members
-        )
-        groups.append(t)
-    return response.json(dict(
-        groups = groups
-    ))
-
-def get_group():
-    r = db(db.group_data.id == request.vars.group_id).select().first()
-    return response.json(dict(group=dict(
-		id = r.id,
-        group_owner = r.group_owner,
-		group_name = r.group_name,
-        members = r.members
-    )))
-
-def invite_member():
-    r = db(db.user_data.email == request.vars.invitee).select().first()
-    if r.notifications is not None:
-            r.notifications.append(request.vars.group_id)
-            r.update_record()
-    else:
-            r.update_record(notifications=[request.vars.group_id])
-    return "ok"
-
-def add_to_group():
-    r = db(db.group_data.id == request.vars.group_id).select().first()
-    if r.members is not None:
-            r.members.append(request.vars.guest)
-            r.update_record()
-    else:
-            r.update_record(members=[request.vars.guest])
